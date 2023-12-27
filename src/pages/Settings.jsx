4a7a32546a1ddfe04c0744340dab0e9e9086c8ce
akorede <emiolasaheed1@gmail.com>
2023-12-27 10:36:37 +0100

    Style settings page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,6 +1,26 @@
 const Settings = () => {
   return (
-    <div>Settings</div>
+    <section  className="py-10 w-[90%] mx-auto">
+      <h2 className=" text-3xl pb-5 font-semibold">Update hotel settings</h2>
+      <form className="bg-white shadow-md p-4" >
+      <section className="grid grid-cols-3 items-center border-b  border-gray-300 py-3">
+        <label htmlFor="min_booking" className=" font-medium">Minimum nights/booking</label>
+        <input type="number"  id="min_booking" className=" border border-gray-300 rounded-md px-3 py-2" value={5}  />
+      </section>
+      <section className="grid grid-cols-3 items-center border-b  border-gray-300 py-3">
+        <label htmlFor="max_booking" className=" font-medium">Maximum nights/booking</label>
+        <input type="number"  id="max_booking" className=" border border-gray-300 rounded-md px-3 py-2" value={5}  />
+      </section>
+      <section className="grid grid-cols-3 items-center border-b  border-gray-300 py-3">
+        <label htmlFor="max_guests" className=" font-medium">Maximum guests/booking</label>
+        <input type="number"  id="max_guests" className=" border border-gray-300 rounded-md px-3 py-2" value={5}  />
+      </section>
+      <section className="grid grid-cols-3 items-center py-3">
+        <label htmlFor="breakfast_price" className=" font-medium">Breakfast price</label>
+        <input type="number"  id="breakfast_price" className=" border border-gray-300 rounded-md px-3 py-2" value={5}  />
+      </section>
+    </form>
+    </section>
   )
 }
 
