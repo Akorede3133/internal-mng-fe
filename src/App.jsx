622e82aa2ac3c0f61cb7ae2c8e466fea8dbf7aa2
akorede <emiolasaheed1@gmail.com>
2023-12-31 12:44:52 +0100

    Create Booking page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import AppLayout from "./components/AppLayout";
 import { createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import Booking from "./pages/Booking";
 import Cabin from "./pages/Cabin";
 import User from "./pages/User";
 import Settings from "./pages/Settings";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
+import Bookings from "./pages/Bookings";
+import Booking from "./pages/Booking";
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route element={<AppLayout />}>
     <Route path="/" element={<Navigate to="dashboard" replace />}  />
     <Route path="dashboard" element={<Dashboard />}/>
 
-    <Route path="bookings" element={<Booking />}/>
+    <Route path="bookings" element={<Bookings />}/>
+    <Route path="booking/:id" element={<Booking />}/>
+
     <Route path="cabins" element={<Cabin />}/>
     <Route path="users" element={<User />}/>
     <Route path="settings" element={<Settings />}/>
--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,28 +1,6 @@
-import BookingsTable from "../components/BookingsTable"
-import Filter from "../components/Filter"
-import Sort from "../components/Sort"
-
 const Booking = () => {
   return (
-    <section className="py-10 w-[90%] mx-auto relative">
-       <div className="flex justify-between items-center my-5">
-        <h2>All Bookings</h2>
-        <div className=" flex items-center gap-4">
-          <Filter type='status' options={[{value: '', label: 'All'}, { value: 'checked-out', label: 'Checked out'}, { value: 'checked-in', label: 'Checked in' },  { value: 'unconfirmed', label: 'Unconfirmed' }]} />
-          <Sort options={[
-              { value: "start_date-desc", label: "Sort by date (recent first)" },
-              { value: "start_date-asc", label: "Sort by date (earlier first)" },
-              {
-                value: "total_price-desc",
-                label: "Sort by amount (high first)",
-              },
-              { value: "total_price-asc", label: "Sort by amount (low first)" },
-            ]} 
-          />
-        </div>
-      </div>
-      <BookingsTable />
-    </section>
+    <div>Booking</div>
   )
 }
 
